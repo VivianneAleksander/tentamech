476084dab8788197df24766696990fe2465f822c
VivianneAleksander <101853852+VivianneAleksander@users.noreply.github.com>
2025-02-06 12:00:32 -0500

    Attempt at integrating Spine2D. Probably a failure.

--- scripts/util/spine_sprite_control.gd
+++ scripts/util/spine_sprite_control.gd
@@ -2,11 +2,14 @@ extends SpineSprite
 class_name SpineSpriteControl
 
 
-func set_animation_state(name : String, looping : bool = false, track : int = 0) -> void:
+func set_animation_state(name : String, looping : bool = false, track : int = 0) -> SpineTrackEntry:
+	if not is_inside_tree(): return
 	var animation_state := get_animation_state()
-	animation_state.set_animation(name, looping, track)
+	var new_track := animation_state.set_animation(name, looping, track)
+	return new_track
 
-func queue_animation_state(name : String, delay : float = 0.5, looping : bool = false, track : int = 0) -> void:
+func queue_animation_state(name : String, delay : float = 0.5, looping : bool = false, track : int = 0) -> SpineTrackEntry:
+	if not is_inside_tree(): return
 	var animation_state := get_animation_state()
-	animation_state.add_animation(name, delay, looping, track)
-	
+	var new_track := animation_state.add_animation(name, delay, looping, track)
+	return new_track
