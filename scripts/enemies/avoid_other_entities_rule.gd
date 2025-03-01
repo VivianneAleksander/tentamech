11e18adb8ffe0ba2cc663a5d85dd9cd9b1d697cb
VivianneAleksander <101853852+VivianneAleksander@users.noreply.github.com>
2025-03-01 13:50:38 -0500

    Shader fixes + AvoidOtherEntitiesRule

--- scripts/enemies/avoid_other_entities_rule.gd
+++ scripts/enemies/avoid_other_entities_rule.gd
@@ -2,6 +2,7 @@ extends AIRule
 class_name AvoidOtherEntitiesRule
 
 @export var max_distance : float = 1.0
+@export var acceleration : float = 1.0
 @export_enum("Bullets", "Enemies", "Player") var type : int = 0
 var type_str : StringName : 
 	get:
@@ -14,15 +15,18 @@ var type_str : StringName :
 				return "Player"
 		return ""
 
+var valid_nodes : Array[Node3D]
+
 func _check_rule(primary : AreaCharacter3D, target : Node3D) -> bool:
-	return true
+	valid_nodes.assign(get_tree().get_nodes_in_group(type_str))
+	return valid_nodes.any(func(node : Node3D) -> bool: return (node.global_position - primary.global_position).length() < max_distance)
 
 func _apply_rule(primary : AreaCharacter3D, target : Node3D) -> Vector3:
 	var vector = Vector3.ZERO
-	for node in get_tree().get_nodes_in_group(type_str):
-		if not (node as Node3D):
+	for node in valid_nodes:
+		if not node:
 			continue
-		var v : Vector3 = (node.global_position - primary.global_position).length()
+		var v : Vector3 = (primary.global_position - node.global_position)
 		if v.length() <= max_distance:
 			vector += v
-	return Vector3.ZERO
+	return vector * acceleration
